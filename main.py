9fdfa419be8dd77731f067ece37dad3ca9adcd60
fulldada <david.joubert@hotmail.fr>
2021-04-09 16:30:21 +0200

    test MaJ

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def getstatus():
 
 if __name__ == '__main__':
     window = Tk()
+    window.minsize(480, 360)
     window.title("Welcome to LikeGeeks app")
-    window.mainloop()
     print(getstatus())
+    username = 'fulldada'
+    print(requests.post('https://api.spacetraders.io/users/' + username + '/token').json())
+    window.mainloop()
+
+    # token : cde3bb98-0e31-4a0b-8041-cf701070a775
+
