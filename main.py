115f8fb373e00a980796ab263721ec8390a7f4fa
Darleenax <watrain.alexandre@hotmail.fr>
2021-06-04 16:07:33 +0200

    test connexion (debut)

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import requests
 from tkinter import *
-
-# TODO
-# - Empty window
-# - Display status
-# - Enter a name
-# - Display player's info
-# - Display list of available ship
-
+import json
 
 def get_status():
     """Check the server's status.
@@ -26,7 +19,8 @@ def get_loan(_token: str):
 
 
 def post_loan(_username: str, _token: str):
-    return requests.post('https://api.spacetraders.io/users/' + _username + '/loans token==' + _token + ' \ type=STARTUP').json()
+    return requests.post(
+        'https://api.spacetraders.io/users/' + _username + '/loans token==' + _token + ' \ type=STARTUP').json()
 
 
 def init_window(_window: Tk):
@@ -39,6 +33,7 @@ def init_window(_window: Tk):
     _window.minsize(720, 480)
     _window.maxsize(720, 480)
     _window.title('sPYceTraders')
+
 
 def frame_login(_window):
     F1 = Frame(_window, bg="blue")
@@ -53,19 +48,25 @@ def frame_login(_window):
     name = Entry(F1, textvariable=F1, justify="center", font=("Comic", 20))
     name.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    button = Button(F1, text="Connect !", font=("Comic", 15))
+    button = Button(F1, text="Connect !", font=("Comic", 15), command=lambda: connect(name.get()))
     button.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+
+def connect(_prout: str):
+    with open('data.txt') as fic:
+        data = json.load(fic)
+        for user in data['users']:
+            print(user['user']['username'])
+
 
 if __name__ == '__main__':
     window = Tk()
     init_window(window)
     frame_login(window)
-    print(get_status())
-    username = 'darleenax'
-    token = 'fb4ba70e-a0ba-4f8e-9152-b4f3ba378dc7'
-    token = '139a6675-2870-4734-89e0-d0aa44d64e81'
-    print(post_loan(username, token))
-    print(post_username(username))
     window.mainloop()
+    print(get_status())
 
-    # token : cde3bb98-0e31-4a0b-8041-cf701070a775
+
+
+    #print(post_loan(username, token))
+    #print(post_username(username))
